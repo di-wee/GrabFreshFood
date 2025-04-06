43953df90d6e75c1fe571b3ef8fd35d651dd758f
Dionis <callmehdion@gmail.com>
2025-04-06 13:29:56 +0800

    shipping details layout frontend completed

--- src/main/java/nus/iss/team1/grabfreshfood/controller/OrderController.java
+++ src/main/java/nus/iss/team1/grabfreshfood/controller/OrderController.java
@@ -1,4 +1,12 @@
 package nus.iss.team1.grabfreshfood.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OrderController {
+    @GetMapping("/checkout")
+    public String checkoutPage() {
+        return "checkout-page";
+    }
 }

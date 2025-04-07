38ecc2d3a8d462e17386f998374b5be35f20d9f6
Dionis <callmehdion@gmail.com>
2025-04-07 11:01:31 +0800

    fix for broken login page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import ShoppingCart from './components/ShoppingCart';
@@ -8,27 +8,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
-	createRoot(rootEl).render(
-		<StrictMode>
-			<App />
-		</StrictMode>
-	);
+    createRoot(rootEl).render(
+        <StrictMode>
+            <App/>
+        </StrictMode>
+    );
 }
 
 const cartEl = document.getElementById('react-cart');
 if (cartEl) {
-	createRoot(cartEl).render(
-		<StrictMode>
-			<ShoppingCart />
-		</StrictMode>
-	);
+    createRoot(cartEl).render(
+        <StrictMode>
+            <ShoppingCart/>
+        </StrictMode>
+    );
 }
 
 const catalogueEl = document.getElementById('react-catalogue');
 if (catalogueEl) {
-	createRoot(catalogueEl).render(
-		<StrictMode>
-			<ProductCatalogue />
-		</StrictMode>
-	);
+    createRoot(catalogueEl).render(
+        <StrictMode>
+            <ProductCatalogue/>
+        </StrictMode>
+    );
 }
+

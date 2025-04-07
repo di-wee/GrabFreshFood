18355e2c5e10b50462a7f07093fbbfe39cd89a5c
Dionis <callmehdion@gmail.com>
2025-04-07 18:43:21 +0800

    Created ShoppingCartRestController

---
+++ src/main/java/nus/iss/team1/grabfreshfood/controller/ShoppingCartRestController.java
@@ -0,0 +1,8 @@
+package nus.iss.team1.grabfreshfood.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShoppingCartRestController {
+    
+}

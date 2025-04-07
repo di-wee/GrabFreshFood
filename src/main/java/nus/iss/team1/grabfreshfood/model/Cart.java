9123dd098aa563256c5a31a2337f1288328a8f0f
Dionis <callmehdion@gmail.com>
2025-04-07 15:17:24 +0800

    Linked ShoppingCartItem and Cart Entities to DB

--- src/main/java/nus/iss/team1/grabfreshfood/model/Cart.java
+++ src/main/java/nus/iss/team1/grabfreshfood/model/Cart.java
@@ -1,64 +1,30 @@
 package nus.iss.team1.grabfreshfood.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cart_id")
-    private int cartId;
+    private Long cartId;
 
     @Column(name = "customer_id")
-    private int customerId;
+    private Long customerId;
 
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
 
     private String status;
 
+    @OneToMany(mappedBy = "cart")
     private List<CartItem> cartItems;
 
-    public int getCartId() {
-        return cartId;
-    }
-
-    public void setCartId(int cartId) {
-        this.cartId = cartId;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<CartItem> getCartItems() {
-        return cartItems;
-    }
-
-    public void setCartItems(List<CartItem> cartItems) {
-        this.cartItems = cartItems;
-    }
 }
---
+++ src/main/java/nus/iss/team1/grabfreshfood/model/CartItem.java
@@ -0,0 +1,33 @@
+package nus.iss.team1.grabfreshfood.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "shopping_cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long cartItemId;
+
+    @Column(name = "product_id")
+    private Long productId;
+    private double price;
+
+    @Column(name = "is_checkout")
+    private boolean isCheckout;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    public CartItem() {
+    }
+
+}

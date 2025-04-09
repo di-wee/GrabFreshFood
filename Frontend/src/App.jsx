d248138a02871ac116b51108bcdb5820d0eeaa94
Dionis <callmehdion@gmail.com>
2025-04-09 10:48:39 +0800

    Added React Component (Product Catalogue) to thyme landing page. And added customerId to session for shopping cart logic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ShoppingCart from './components/ShoppingCart';
+import ProductCatalogue from './components/Productcatalogue';
 const App = () => {
 	//logic to retrieve and call GET API to retrieve Products
 	// afterwards to loop and prop down to Product card
 
 	return (
 		<>
-			<ShoppingCart />
+			<ProductCatalogue />
 		</>
 	);
 };
--- Frontend/src/components/ProductCatalogue.jsx
+++ Frontend/src/components/ProductCatalogue.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const ProductCatalogue = () => {
+	const [isAddCart, setIsAddCart] = useState(false);
+	const [itemQuant, setItemQuant] = useState(1);
+
+	function handleAddCart() {
+		//add to cart logic goes here
+		//REST api to GET product information and then to apply .map to each product card
+
+		//to toggle visibility
+		setIsAddCart(true);
+	}
+
+	//to handle functionality of increasing/decreasing item quantity
+	function handleItemQuant(e) {
+		const quant = parseInt(e.target.value);
+
+		//if quantity === 0, then it will set boolean flag isAddCart to false to trigger visibility
+		if (quant === 0) {
+			setIsAddCart(false);
+			console.log('isAddCart: ' + isAddCart);
+			console.log('itemQuant: ' + quant);
+		}
+		setItemQuant(quant);
+	}
+
 	return (
 		<div className='container'>
-			<div
-				className='row justify-content-center align-items-center'
-				style={{ height: '100vh' }} // full screen height
-			>
+			<div className='row justify-content-center align-items-center'>
 				<div className='col-md-4'>
 					<Card>
 						<Card.Img
@@ -18,7 +39,20 @@ const ProductCatalogue = () => {
 						<Card.Body style={{ width: '250vh' }}>
 							<Card.Title>Product Title</Card.Title>
 							<Card.Text>Product Description</Card.Text>
-							<Button variant='primary'>Add to cart</Button>
+							<Card.Text>$ProductPrice</Card.Text>
+							{isAddCart ? (
+								<input
+									type='number'
+									min={0}
+									value={itemQuant}
+									onChange={(e) => handleItemQuant(e)}></input>
+							) : (
+								<Button
+									onClick={handleAddCart}
+									variant='success'>
+									Add to cart
+								</Button>
+							)}
 						</Card.Body>
 					</Card>
 				</div>

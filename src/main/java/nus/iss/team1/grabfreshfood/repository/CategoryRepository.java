65b3891ecdedf5221ea07cf8b9b87e902f2ee213
Dionis <callmehdion@gmail.com>
2025-04-12 11:38:06 +0800

    Revert "Merge pull request #29 from slzwei/shawn-navbar-update"
    This reverts commit 1a72c2322a076bdbda4968ec2888620afee76ccf, reversing
    changes made to 19f3c86d103ae0bf165bdf7ccb801140f24a3f00.

--- src/main/java/nus/iss/team1/grabfreshfood/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package nus.iss.team1.grabfreshfood.repository;
-
-import nus.iss.team1.grabfreshfood.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- src/main/java/nus/iss/team1/grabfreshfood/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package nus.iss.team1.grabfreshfood.service;
-
-import nus.iss.team1.grabfreshfood.model.Category;
-import java.util.List;
-
-public interface CategoryService {
-    List<Category> getAllCategoriesWithSubcategories();
-}

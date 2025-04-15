5574d1fbcecb9878573ae94eec6c3d2e746c3c08
Ben Yen <Benjamin.x.yen@gmail.com>
2025-04-15 16:48:07 +0700

    Resolved merge conflict: kept findByName as confirmed and reverted to local change

--- src/main/java/nus/iss/team1/grabfreshfood/repository/CategoryRepository.java
+++ src/main/java/nus/iss/team1/grabfreshfood/repository/CategoryRepository.java
@@ -1,11 +1,8 @@
 package nus.iss.team1.grabfreshfood.repository;
 
 import nus.iss.team1.grabfreshfood.model.Category;
-import nus.iss.team1.grabfreshfood.model.ProductCategories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {

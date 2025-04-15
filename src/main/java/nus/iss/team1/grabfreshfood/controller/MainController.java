b2f9ac518a2bd07db8dc452cea25f7d817d3620f
slzwei <shawnleeapps@gmail.com>
2025-04-15 16:28:35 +0800

    1) Modified using shiying's code for categoryimpl, categoryrepository to accurately link the category items.
    2) Navbar now has a dropdown for the account, showing the account page, order history page, and logout page.

--- src/main/java/nus/iss/team1/grabfreshfood/controller/MainController.java
+++ src/main/java/nus/iss/team1/grabfreshfood/controller/MainController.java
@@ -2,16 +2,27 @@
 
 package nus.iss.team1.grabfreshfood.controller;
 
+import nus.iss.team1.grabfreshfood.model.Category;
+import nus.iss.team1.grabfreshfood.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
-
+    @Autowired
+    private CategoryService categoryService;
     // Main landing page
     @GetMapping("/")
-    public String landingPage() {
+    public String landingPage(Model model) {
+        List<Category> categories=categoryService.getAllCategoriesWithSubcategories();
+        model.addAttribute("categories", categories);
+
         return "landing-page";
+
     }
 
 }
